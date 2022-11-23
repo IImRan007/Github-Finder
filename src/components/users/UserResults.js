067d4c8b51f1f1cdac33a1a028310c7a6c8b0c97
Mohd Imran <mohd78696321@gmail.com>
2022-11-23 12:13:51 +0530

    Added UserItem Component

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -24,9 +25,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <div key={user.id}>
-            <h3>{user.login}</h3>
-          </div>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
